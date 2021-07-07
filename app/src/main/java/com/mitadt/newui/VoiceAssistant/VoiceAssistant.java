a29b8c5774b8fec7e93789908520725d1ca902ea
itssaiii <abhisheksaigiridhari@gmail.com>
2021-07-07 09:24:57 +0530

    "Updated Voice Assistant"

--- app/src/main/java/com/mitadt/newui/VoiceAssistant/VoiceAssistant.java
+++ app/src/main/java/com/mitadt/newui/VoiceAssistant/VoiceAssistant.java
@@ -3,6 +3,7 @@ package com.mitadt.newui.VoiceAssistant;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.mitadt.newui.ComputerScience.ZoomLecture;
+import com.mitadt.newui.DiscussionForum.AddQuestion;
 import com.mitadt.newui.DiscussionForum.ViewAllQuestions;
 import com.mitadt.newui.LogInOrSignUp;
 import com.mitadt.newui.R;
@@ -25,7 +27,9 @@ public class VoiceAssistant extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
     ImageView voice;
-    TextView command;
+    TextView command,nextcommand;
+    TextToSpeech t1;
+    public String next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +39,23 @@ public class VoiceAssistant extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         command = findViewById(R.id.command);
         command.setVisibility(View.INVISIBLE);
+        nextcommand = findViewById(R.id.nextcommand);
+        nextcommand.setVisibility(View.INVISIBLE);
+
 
         voice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 bolnabhai();
+            }
+        });
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status!=TextToSpeech.ERROR){
+                    t1.setLanguage(Locale.UK);
+                }
             }
         });
 
@@ -51,6 +67,7 @@ public class VoiceAssistant extends AppCompatActivity {
 
         Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS,6000);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hukum Jahapanah");
         try
@@ -77,7 +94,8 @@ public class VoiceAssistant extends AppCompatActivity {
             String first = arrSplit[0] + " " + arrSplit[1];
             command.setText(first);
 
-
+            datastructures(command);
+            postquestion(command);
             if(command.getText().toString().equals("open books")) {
                 startActivity(new Intent(getApplicationContext(), ViewAdsaBooks.class));
 
@@ -95,18 +113,45 @@ public class VoiceAssistant extends AppCompatActivity {
             }
             else if(command.getText().toString().equals("join lecture"))
             {
-                startActivity(new Intent(getApplicationContext(), ZoomLecture.class));
+                nextcommand.setText("Which Lecture");
+                String toSpeak = nextcommand.getText().toString().trim();
+                Toast.makeText(this, toSpeak, Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
+                bolnabhai();
+
+
+            }
+            else if(command.getText().toString().equals("ask question")){
+                nextcommand.setText("Which Subject");
+                String toSpeak = nextcommand.getText().toString().trim();
+                Toast.makeText(this, toSpeak, Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
+                bolnabhai();
+
             }
             else {
                 Toast.makeText(this, "Invalid Command", Toast.LENGTH_SHORT).show();
             }
 
+        }
+    }
 
+    private void postquestion(TextView command) {
+        if(command.getText().toString().equals("data communication"))
+        {
 
-
+            startActivity(new Intent(getApplicationContext(), AddQuestion.class));
         }
 
 
+    }
+
+    private void datastructures(TextView command) {
+        if(command.getText().toString().equals("data structures"))
+        {
+
+            startActivity(new Intent(getApplicationContext(), ZoomLecture.class));
+        }
 
 
     }
